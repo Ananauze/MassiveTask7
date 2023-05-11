056359efe7a3d481c10f5bc1ea8bb6f7574be99d
anananass <ananauze@gmail.com>
2023-05-11 13:11:52 +0400

    Add project files.

---
+++ MassiveTask7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MassiveTask7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            string phrase = "Сегодня четверг, а хочется лето.";
+            string[] words = phrase.Split(' ');
+
+            Console.WriteLine("\nВыводим фразу для проверки: \n");
+            Console.WriteLine(phrase);
+
+            Console.WriteLine("\nТеперь разделим по словам: \n");
+            
+            foreach (string word in words) 
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine(word);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
